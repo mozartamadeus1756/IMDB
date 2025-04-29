86bd8d2165697dd39326ead64bba6985f65cc4f3
kine <kine.sandvik07@gmail.com>
2025-04-29 10:29:04 +0200

    made an about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import BackButton from "../BackButton.svelte";
+</script>
+
+<BackButton />
+<div class="about-container">
+    <h1>About</h1>
+    <p>This is the about page of the app.</p>
+    <p>Here you can find information about the app and its features.</p>
+    <p>For more information, visit our website.</p>
+</div>
