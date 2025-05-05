35d98de787eab8f01ee0841c747c944f4974dfe7
kine <kine.sandvik07@gmail.com>
2025-05-05 11:00:05 +0200

    install some dependencies

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -6,7 +6,7 @@
             <li><a href="/">IMDB</a></li>
             <li><a href="/search">Search</a></li>
             <li><a href="/randomise">Randomise</a></li>
-            <li><a href="/register">Register</a></li>
+            <li><a href="/register">Login</a></li>
         </ul>
     </div>
 </nav>
